e8956b3b36d09416329fd94b132e43c6eb5cf088
supreethmsms <supreethrahul@gmail.com>
2018-12-28 15:35:40 +0530

    fivth commit

---
+++ src/Practice/Sample.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i;
+		i=10;
+		System.out.println(i);
+	}
+
+}
